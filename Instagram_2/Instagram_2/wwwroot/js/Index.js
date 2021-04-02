b92811dded5e016f8689b5d7ea1fdfe8ce300ad3
Marcos Grijalva <shonish04@gmail.com>
2021-04-01 19:16:09 -0600

    Instagram_2

---
+++ Instagram_2/Instagram_2/wwwroot/js/Index.js
@@ -0,0 +1,9 @@
+﻿var div = document.getElementById("MainContent");
+
+var newSpan = $("<span></span>").text("Hello 2");
+$("#MainContent").append(newSpan);
+
+
+$("#MainContent").on("click", ".ButtonClass", ClickButton);
+
+$("<span>Test</span>").append(".ParaClass"); 
